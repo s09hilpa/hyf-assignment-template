b5e158612c6ca59719fa8446f08a3132e64e1116
Shilpa Jumde <shilpakhapre88@gmail.com>
2026-01-07 20:49:55 +0100

    assignment frontend

---
+++ hyfBayHelpers.js
@@ -0,0 +1,25 @@
+function getAvailableProducts() {
+  return [
+    {
+      title: "Rice (5kg)",
+      price: 18,
+      rating: 4.5,
+      image:
+        "https://assets.bonappetit.com/photos/59e77b15c8947d766a9664e5/16:9/w_2560%2Cc_limit/basmati-rice.png",
+    },
+    {
+      title: "Cooking Oil (2L)",
+      price: 20,
+      rating: 4.3,
+      image:
+        "https://www.heart.org/en/-/media/AHA/Recipe/Article-Images/healthy-cooking-oils.png?sc_lang=en",
+    },
+    {
+      title: "Milk (1L)",
+      price: 20,
+      rating: 4.7,
+      image:
+        "https://img.freepik.com/free-vector/flat-design-lactose-free-horizontal-banner_23-2151177371.jpg?semt=ais_hybrid&w=740&q=80",
+    },
+  ];
+}
