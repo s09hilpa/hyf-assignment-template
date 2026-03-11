cf9210e9b45aabca46584571cb987d7061921b08
Shilpa Jumde <shilpakhapre88@gmail.com>
2026-03-11 21:46:48 +0100

    Exercise 3: Low stock alert with filter map and sort

---
+++ exercise3.js
@@ -0,0 +1,8 @@
+import { teas } from "./teas.js";
+function lowStockAlert(teas) {
+  return teas
+    .filter((tea) => tea.stockCount < 50)
+    .map((tea) => ({ name: tea.name, stockCount: tea.stockCount }))
+    .sort((a, b) => a.stockCount - b.stockCount);
+}
+console.log(lowStockAlert(teas));
